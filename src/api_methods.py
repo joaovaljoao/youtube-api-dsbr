142320462891804945e67737176ed76933988c11
João Carlos <joao.oliveira@ufob.edu.br>
2022-04-15 11:48:32 -0300

    Criando lógica para parsear URLs

--- src/api_methods.py
+++ src/api_methods.py
@@ -1,4 +1,5 @@
 from requests import get as GET
+import json
 
 class YoutubeApi:
 
@@ -9,8 +10,17 @@ class YoutubeApi:
     
     def get_channel_id(self, channel_name:str) -> str:
         params = {
-            'part': 'snippet',
+            'part': 'id',
             'forUsername': channel_name,
+            'key': self.api_key
+        }
+        response = GET(self.url + 'channels', params=params)
+        return response.json()
+
+    def get_channel_info(self, channel_id:str) -> dict:
+        params = {
+            'part': 'snippet,contentDetails,statistics',
+            'id': channel_id,
             'key': self.api_key
         }
         response = GET(self.url + 'channels', params=params, headers=self.headers)
@@ -25,13 +35,13 @@ class YoutubeApi:
         response = GET(self.url + 'videos', params=params, headers=self.headers)
         return response.json()
     
-    def get_content(self, channel_key:str) -> dict:
+    def get_content(self, channel_url:str) -> dict:
 
-        for item in youtube_url:
-            key = item.split("/")[-1]
-            if item.split("/")[-2] != 'channel':
-                channel_id = self.get_channel_id(key)
-                return self.get_videos(channel_id)
-            else:
-                return self.get_videos(key)
-
+        key = channel_url.split("/")[-1]
+        if channel_url.split("/")[-2] == 'channel':
+            return self.get_channel_info(key)
+        elif channel_url.split("/")[-2] == 'user':
+            channel_id = self.get_channel_id(key)['items'][0]['id']
+            return self.get_channel_info(channel_id)
+        else:
+            return None
